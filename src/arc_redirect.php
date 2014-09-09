84a54c99a12453e6c3aef697989780c27b8ccb5a
Andy Carter <andy1704@gmail.com>
2014-09-09 19:39:12 +0100

    Updated version number to 1.2.0

--- src/arc_redirect.php
+++ src/arc_redirect.php
@@ -1,7 +1,7 @@
 <?php
 $plugin['name'] = 'arc_redirect';
 
-$plugin['version'] = '1.1';
+$plugin['version'] = '1.2.0';
 $plugin['author'] = 'Andy Carter';
 $plugin['author_uri'] = 'http://andy-carter.com/';
 $plugin['description'] = 'Love redirects, hate 404s';
